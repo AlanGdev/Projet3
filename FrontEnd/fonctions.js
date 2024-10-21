e674fe268a99516960bc257dca3e06d6599de14c
AlanGdev <alan.grolleau@gmail.com>
2024-10-21 11:52:42 +0200

    Simplification du code avec les fonctions

--- FrontEnd/fonctions.js
+++ FrontEnd/fonctions.js
@@ -2,7 +2,7 @@ export function effacerContenuBalise(balise){
     balise.innerHTML=''
 }
 
-export function afficherProjets(projets,balise){
+export function afficherProjets(projets,baliseAffichage){
     //Pour chaque element du tableau:
     projets.forEach(item=>{
         // On crée une balise <figure>:
@@ -13,7 +13,62 @@ export function afficherProjets(projets,balise){
             <figcaption>${item.title}</figcaption>
             `
         // On ajoute cette balise au contenu de la classe parente galleryClass
-        balise.appendChild(figureBal)
+        baliseAffichage.appendChild(figureBal)
     })
 }
 
+export function switchClassFilterBtn(btn,newClass){
+    btn.className="" // On efface les classes présentes sur le bouton
+    btn.classList.add(newClass) // On lui rajoute la classe passée en argument de la fonction
+}
+
+export function createBtnFilterTous(classe){
+    // On crée le bouton dejà cliqué (filter-btn-clicked) - attribut data-id="Tous"
+    classe.innerHTML=`
+    <button data-id="Tous" class="filter-btn-clicked">Tous</button>
+    `
+}
+
+export function createBtnFilter(parent,categorie){
+    const btn=document.createElement("button") //On crée un élément button
+    btn.setAttribute("data-id",`${categorie.id}`) // on ajoute un attribut: data-id=categorie.id
+    btn.innerText=`${categorie.name}` //on ajoute du texte: categorie.name
+    btn.classList.add("filter-btn") //on ajoute une classe pour mise en forme du bouton (non cliqué)
+    parent.appendChild(btn) // on ajoute le bouton créé à son parent ('filtersClass')
+}
+
+export function addEventListenerButtonFilter(classeBoutons,projets,classeProjets){
+    const filters=classeBoutons.querySelectorAll("button") //On sélectionne tous les boutons de la classe
+    //On lance une boucle forEach pour chaque bouton auxquels on va ajouter un gestionnaire d'évènements
+    filters.forEach(filterbtn=>
+        filterbtn.addEventListener("click",()=>{
+            //On repasse tous le bouton anciennement clicqué (filter-btn-clicked) en non cliqué (classe filter-btn):
+            filters.forEach(filterbtn=>{
+                // Si le bouton est cliqué
+                if(filterbtn.classList.contains("filter-btn-clicked")){
+                    //On le passe en non cliqué
+                    switchClassFilterBtn(filterbtn,"filter-btn")
+                }
+            })
+            //On passe la classe du bouton cliqué en "filter-btn-clicked"
+            switchClassFilterBtn(filterbtn,"filter-btn-clicked")
+            // On enregistre la catégorie correspondant au bouton dans la variable id
+            let id=filterbtn.dataset.id
+            // On crée la liste des projets filtrés:
+            const filteredProjects=(()=>{
+                if(id!="Tous"){
+                    return projets.filter((projet)=>{
+                        return projet.category.id===Number(id)
+                    })
+                }
+                else{
+                    return[...projets] //si l'id="Tous", on renvoie une copie du tableau projets
+                }
+            })() // Les parenthèses de fin lancent la fonction
+            // On efface le contenu du conteneur des projets:
+            effacerContenuBalise(classeProjets)
+            // On affiche les projets filtrés:
+            afficherProjets(filteredProjects,classeProjets)
+        })
+    )
+}
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -9,75 +9,27 @@ const projects=await respProjects.json() // projets: tableau des travaux au form
  const categories= await respCategories.json() //categories: tableau des catégories au format json
  
 // Récupération de la class "gallery" dans la page HTML (C'est là que sont affichés les projets)
-let galleryClass=document.querySelector(".gallery")
+let gallery=document.querySelector(".gallery")
 
-// Efface le contenu de la div class='gallery' pour effacer les travaux en static via la focntion importée
-fonction.effacerContenuBalise(galleryClass)
+// Efface le contenu de gallery pour effacer les travaux en static via la focntion importée
+fonction.effacerContenuBalise(gallery)
 
 // Affichage des projets via la fonction importée
-fonction.afficherProjets(projects,galleryClass)
+fonction.afficherProjets(projects,gallery)
 
  //Récupération de la class div class="filters" dans la page HTML (c'est là que sont affichés les filtres)
  let filtersClass=document.querySelector(".filters")
 
- // Efface le contenu de la div class='filters' pour effacer les filtres en static
- filtersClass.innerHTML=''
+ //Création du bouton Tous (toutes catégories) avec de base la classe filter-btn-clicked (bouton clické) et le data-id="Tous"
+fonction.createBtnFilterTous(filtersClass)
 
- //Création du bouton Tous (toutes catégories) avec de base la classe filter-btn-clicked et le data-id="0"
- filtersClass.innerHTML=`
-    <button data-id="0" class="filter-btn-clicked">Tous</button>
-    `
-
- // Pour chaque élément de categories:
+ // Pour chaque élément de categories: Création d'un bouton (avec id et class "filter-btn" (non cliqué))
  categories.forEach(categorie=>{
-    const btn=document.createElement("button") //On crée un élément button
-    btn.setAttribute("data-id",`${categorie.id}`) // on ajoute un attribut: data-id=categorie.id
-    btn.innerText=`${categorie.name}` //on ajoute du texte: categorie.name
-    btn.classList.add("filter-btn") //on ajoute une classe pour mise en forme du bouton (non cliqué)
-    filtersClass.appendChild(btn) // on ajoute le bouton créé à son parent ('filtersClass')
+    fonction.createBtnFilter(filtersClass,categorie)
  })
 
  //On va rajouter un gestionnaire d'évenements sur les boutons, au click
- const filters=filtersClass.querySelectorAll("button") //=>On selectionne tous les boutons
- //On lance une boucle forEach sur chaque boutton:
-filters.forEach(filter=>{
-    //on prend le bouton et on rajoute le gestionnaire d'évenement au click - On ouvre une fonction flêchée:
-    filter.addEventListener("click",()=>{
-            /*On va retirer la classe filter-btn-clicked du bouton cliqué antérieurement pour l'ajouter au nouveau bouton cliqué ensuite
-            //On vérifie donc chaque bouton pour enlevé la classe cliqué si besoin et tous les remettre comme 'non cliqués'*/
-            filters.forEach(button=>{
-                if (button.classList.contains("filter-btn-clicked")){ // Si le bouton est cliqué (classe filter-btn-clicked:
-                    button.classList.remove("filter-btn-clicked") // On enlève la classe filter-btn-clicked (cliqué)
-                    button.classList.add("filter-btn")// On ajoute la classe filter-btn (normal)
-                    }
-                })
+ fonction.addEventListenerButtonFilter(filtersClass,projects,gallery)
 
-            // On va maintenant s'intéresser au bouton cliqué:
-            filter.classList.add("filter-btn-clicked")// Changement de classe du bouton cliqué (passage en filter-btn-clicked)
-            filter.classList.remove("filter-btn")
-
-            // On enregistre dans une variable, le data-id du bouton, correspondant à la catégorie.
-            let id=Number(filter.dataset.id)
-
-            // On crée la constante filteredProjects:
-            const filteredProjects=(()=>{
-                console.log("On est sur la construction de la constante filteredProjects")
-                if(id!=0){ // Si id != 0 (bouton Tous)
-                    console.log(id)
-                    return projects.filter((project)=>{
-                        return project.category.id===id
-                    })
-                }
-                else{
-                    console.log(id)
-                    return[...projects] // Si id=0, on a tous les projets
-                }
-            })()// Les parenthèses de fin lancent la fonction définie à l'instant.
-
-            fonction.effacerContenuBalise(galleryClass)// On efface le contenu du conteneur (class gallery)
-
-            fonction.afficherProjets(filteredProjects,galleryClass) // on ajoute le contenu des projets filtrés (ou non)
-        })
-    })
 
 
