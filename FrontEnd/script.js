741401298ccc185e64aa99fbb0928bb276ec7eec
AlanGdev <alan.grolleau@gmail.com>
2024-10-28 15:34:04 +0100

    Construction de la page d'édition

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,84 +1,60 @@
 import * as fonction from './fonctions.js'
-async function buildIndexPage(){
-   // Envoi d'un fetch pour récup. des différentes catégories
-   const respCategories=await fetch ('http://localhost:5678/api/categories')
-   const categories= await respCategories.json() //categories: tableau des catégories au format json
-   //Récupération du conteneur div class="filters" dans la page HTML (c'est là que sont affichés les filtres)
-   const filtersClass=document.querySelector(".filters")
 
-   //Création du bouton Tous (toutes catégories) avec de base la classe filter-btn-clicked (bouton clické) et le data-id="Tous"
-   fonction.createBtnFilterTous(filtersClass)
-
-   // Pour chaque élément de categories: Création d'un bouton (avec id et class "filter-btn" (non cliqué))
-   categories.forEach(categorie=>{
-      fonction.createBtnFilter(filtersClass,categorie)
-   })
-   /******** Par défaut, tous les projets sont affichés et le filtre "Tous" est cliqué ******/
-
-   //On rajoute un gestionnaire d'évenements sur les boutons, au click
-   fonction.addEventListenerButtonFilter(filtersClass,projects,gallery)
-
-
-
-}
-
-function buildEditionPage(){
-   //Construction de la page d'édition
-}
-
-/***Pour construction de la page, vérification si token existant et valide */
-//Désignation d'une variable editionPage (pour savoir si on est sur page d'édition) - Par défaut=false
-let editionPage=false
 //verification si token
    const token=localStorage.getItem('token')
    console.log(token)
-//vérification si token valide
-   const response=await fetch('http://localhost:5678/api/works',{
-      method:"POST",
-      headers:{
-         "Authorization":`Bearer ${token}`,
-         "Content-Type":"application/json"
-      }
-   })
-   if(response.status===401){
-      console.log("Erreur d'authentification / Token absent ou invalide")
-      editionPage=false
-   }
-   else{
-      editionPage=true
-   }
 
 // Envoi d'un fetch pour récup. des travaux sur l'API
 const respProjects=await fetch('http://localhost:5678/api/works')
 const projects=await respProjects.json()
 
-// Envoi d'un fetch pour récup. des différentes catégories
+// Envoi d'un fetch pour récup. des catégories
 const respCategories=await fetch ('http://localhost:5678/api/categories')
 const categories= await respCategories.json()
+
+const editionMode=document.getElementById("edition-mode")
+const header=document.getElementById("header")
+const log=document.getElementById("log")
+const modifier=document.getElementById("modifier")
+const filters=document.getElementById("filters")
  
 const gallery=document.querySelector(".gallery") //Récup. du conteneur gallery pour affichage projets
 fonction.effacerContenuBalise(gallery)
 fonction.afficherProjets(projects,gallery)
 
-if (!editionPage){
-   //Récupération du conteneur div class="filters" dans la page HTML (c'est là que sont affichés les filtres)
-   const filtersClass=document.querySelector(".filters")
+if (!token){
+   editionMode.classList.add("hidden")
+   modifier.classList.add("hidden")
+   //Récupération du conteneur "filters" dans la page HTML (c'est là que sont affichés les filtres)
 
    //Création du bouton Tous (toutes catégories) avec de base la classe filter-btn-clicked (bouton clické) et le data-id="Tous"
-   fonction.createBtnFilterTous(filtersClass)
+   fonction.createBtnFilterTous(filters)
 
    // Pour chaque élément de categories: Création d'un bouton (avec id et class "filter-btn" (non cliqué))
    categories.forEach(categorie=>{
-      fonction.createBtnFilter(filtersClass,categorie)
+      fonction.createBtnFilter(filters,categorie)
    })
-   /******** Par défaut, tous les projets sont affichés et le filtre "Tous" est cliqué ******/
 
    //On rajoute un gestionnaire d'évenements sur les boutons, au click
-   fonction.addEventListenerButtonFilter(filtersClass,projects,gallery)
+   fonction.addEventListenerButtonFilter(filters,projects,gallery)
 }
 else{
-   const hautPage=document.getElementById("edition-mode")
-   hautPage.classList.remove("hidden")
+   editionMode.classList.remove("hidden") // apparition "Mode edition"
+
+   header.classList.add("marge-top-110")// Marge-top header
+
+   modifier.classList.remove("hidden") // apparition "modifier"
+   modifier.addEventListener("click",()=>{
+      console.log("coucou")
+   })
+
+   filters.classList.add("hidden")
+
+   log.innerText="logout" // changement login en logout
+   log.setAttribute("href","index.html")
+   log.addEventListener("click",()=>{
+      localStorage.removeItem("token")
+   })   
 }
 
 
